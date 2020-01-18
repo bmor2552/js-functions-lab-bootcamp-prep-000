d2b90cf958b4deab6a4683ba8f7746b3c5f07e48
Boi Moriba <boi.moriba@gmail.com>
2020-01-18 19:49:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays () {
+  return 'Happy holidays!'
+}
+function happyHolidaysTo('Bob'){
+  return `Happy holidays, ${name}!`
+}
+function happyHolidayTo('Christmas', 'Bob'){
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown('25', 'Christmas') {
+  return `It\'s ${days} until ${holiday}!`
+}
